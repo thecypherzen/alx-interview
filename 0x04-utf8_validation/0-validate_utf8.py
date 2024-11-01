32d71d5fe3e72318ca85c746b9a2476190b23356
= <=>
2024-11-01 15:30:17 +0000

    fix(0x04:0): fix code base logic error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,10 +4,30 @@
 
 def validUTF8(data: list) -> bool:
     """Validates a list of bytes as valid utf-8"""
+    byte_count = 0
+    index_i = 0
+    max_len = len(data)
+    while index_i < max_len:
+        # get least significant byte
+        lsbs = data[index_i] & 0xff
 
-    try:
-        b_data = bytes(data)
-        values = b_data.decode("utf-8")
-        return True
-    except ValueError:
-        return False
+        # check if multi-bit is set set and determine number of
+        # trailing bytes
+        if lsbs >> 7:
+            if lsbs >> 5 == 0b110:
+                byte_count = 1
+            elif lsbs >> 4 == 0b1110:
+                byte_count = 2
+            elif lsbs >> 3 == 0b11110:
+                byte_count = 3
+            else:
+                return False
+
+            # check if following bytes begin with a leading 10
+            while byte_count:
+                index_i += 1
+                if data[index_i] >> 6 != 0b10:
+                    return False
+                byte_count -= 1
+        index_i += 1
+    return True
