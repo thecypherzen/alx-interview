917dc9a7c69a7ce1f336bf792eef0331ddb1f45d
= <=>
2024-11-21 03:58:00 +0000

    build(0x07): add function and test

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""2D array rotation script
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """A function that rotates a 2D n x n array
+    90deg clockwise
+
+    Params:
+       - matrix(list[list[Any]]): n x n matrix
+    Returns:
+       - None
+    """
+    # for clockwise rotation, swap rows
+    # for anti-clockwise rotation, swap columns
+    N = len(matrix)
+    i = 0
+    j = N - 1
+    # swap rows
+    while i < j:
+        temp = matrix[i]
+        matrix[i] = matrix[j]
+        matrix[j] = temp
+        i += 1
+        j -= 1
+    # transpose matrix
+    i = 0
+    while i < N:
+        j = i + 1
+        while j < N:
+            matrix[i][j] = matrix[i][j] ^ matrix[j][i]
+            matrix[j][i] = matrix[i][j] ^ matrix[j][i]
+            matrix[i][j] = matrix[i][j] ^ matrix[j][i]
+            j += 1
+        i += 1
